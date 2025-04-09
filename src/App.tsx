0216f6bf157eeaf7a1d13709175100f8bda773b1
Suhaani Garg <suhaanigarg2311@gmail.com>
2025-04-09 21:46:07 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-4xl font-bold text-blue-600">
+      Hello from MartApp 💙
+    </div>
+  );
+}
+
+export default App;
